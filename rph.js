e0785e41358bac3765113625c8f137fa880d0350
rhubba4863 <rph_1994@yahoo.com>
2023-04-23 13:40:49 -0400

    Finished Beginning Activity

---
+++ rph.js
@@ -0,0 +1,19 @@
+
+//notes
+/*
+  controllers: deliver a response to an incoming request. brain of the opperation
+  database: holds file on how the application will interact with the database server
+    Also will store SQL files
+  model: carry out 
+  public: where static files are stored (not dynamic)
+  route: describes a path that leads to a controller to process an incoming request
+  utilities: functions stored in files that contains reusable code that doesnt fall neatly into the MVC
+  view: dynamic web page that is built by the server (delivered to a browser to be "viewed" by the client)
+  .env: where environnment variables are stored (and store sensitive values)
+  .gitignore: list files and folders not uploaded to github
+  package.json: is some of resources needed in order for project to function
+  README.md: is a markdown file used to describe a github repository
+  server.js: is project server. It describes how the application will work
+*/
+/*
+*/
